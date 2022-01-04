bd49ff1f3a3cab388fc76317a1d29a592f8d1fe9
Anton Petrov <nesamurai@yandex.ru>
2022-01-04 17:02:20 +0300

    created test using apirequestfactory

--- tudu_service/users/tests.py
+++ tudu_service/users/tests.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User as U
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
 
-# Create your tests here.
+from users.models import User
+from users.views import UserModelViewSet
+
+
+class TestUserModelViewSet(TestCase):
+
+    def test_get_users(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        admin = U.objects.create_superuser('anton', 'anton@gb.ru', 'fqrbljrf')
+        force_authenticate(request, admin)
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
