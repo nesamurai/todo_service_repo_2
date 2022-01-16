80920fcb9c7564cd57c0791b5d3b91a7dc0e70be
Anton Petrov <nesamurai@yandex.ru>
2022-01-16 16:26:04 +0300

    included openapi to project

--- tudu_service/tudu_service/urls.py
+++ tudu_service/tudu_service/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from notes.views import ProjectModelViewSet, TuduModelViewSet
 from users.views import UserModelViewSet
@@ -27,11 +30,24 @@ router.register('users', UserModelViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todos', TuduModelViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Tudu_API',
+        default_version='v1',
+        description='Documentation for project',
+        contact=openapi.Contact(email='admin@gb_test.ru'),
+        license=openapi.License(name='MIT License')
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('api/', include(router.urls)),
     path('api/v1/users/', include('users.urls', namespace='v1')),
-    path('api/v2/users/', include('users.urls', namespace='v2'))
+    path('api/v2/users/', include('users.urls', namespace='v2')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0))
 ]
