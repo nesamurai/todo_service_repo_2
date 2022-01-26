f917159e8b2e34843a529b880fb0dc308e878a6b
Anton Petrov <nesamurai@yandex.ru>
2022-01-26 21:15:28 +0300

    using docker for initializing db

---
+++ tudu_service/tudu_service/dev_db_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tudu_service',
+        'USER': 'anton',
+        'PASSWORD': 'fqrbljrf',
+        'HOST': '127.0.0.1',
+        'PORT': '54325'
+    }
+}
